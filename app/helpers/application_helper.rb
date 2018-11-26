f092d89fa59ac411feda8afe31c12f3ea397052c
Lisa Mae <lisamae135b7@me.com>
2018-11-25 20:26:57 -0500

    inital file layout

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+
+
+  module ApplicationHelper
+  # this method will embed the code from the partial
+  def youtube_video(url)
+    render :partial => 'shared/video', :locals => { :url => url }
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+    get '/' => 'home#index', as: :root
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :videos, only: [:index, :new, :create]
+root to: 'videos#index'
+end
