30ecb0a7ba832c894b3c6e70e68afc7107146c4d
Lisa Mae <lisamae135b7@me.com>
2019-01-05 21:18:41 -0500

    added new news page to dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     get '/' => 'home#index', as: :root
+    get '/news' => 'home#news'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :new, :create]
 root to: 'videos#index'
