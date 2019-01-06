9b5b13548ef9f7bc6427d1ae8887f3133a19a140
Lisa Mae <lisamae135b7@me.com>
2019-01-05 21:40:49 -0500

    added repertoire page to dropdown menu route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     get '/' => 'home#index', as: :root
     get '/news' => 'home#news'
+    get '/repertoire' => 'home#repertoire'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :new, :create]
 root to: 'videos#index'
