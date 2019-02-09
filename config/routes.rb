4f21eb2f9acaed534475b8ded125d77e09f73311
Lisa Mae <lisamae135b7@me.com>
2019-02-09 16:18:20 -0500

    Adding OTT integration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     get '/' => 'home#index', as: :root
     get '/news' => 'home#news'
     get '/repertoire' => 'home#repertoire'
+    get '/beats' => 'home#beats'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :new, :create]
 root to: 'videos#index'
